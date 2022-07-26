14c5540ab25af92b7c606ade1642147fde50438c
Bezik1 <mateusz.adamowicz.lo1@gmail.com>
2022-07-26 15:45:38 +0200

    Initial commit

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom'
+import './Navbar.css'
+
+const Navbar = () =>{
+    return (
+        <div className='nav' id='nav'>
+            <div className='nav-links'>
+                <Link  to='/home' className='nav-link' > Home </Link>
+                <Link  to='/aliens' className='nav-link' > Aliens </Link>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
---
+++ src/components/Queen/Queen.tsx
@@ -0,0 +1,9 @@
+const Queen = () =>{
+    return (
+        <div className="queen">
+            
+        </div>
+    )
+}
+
+export default Queen
